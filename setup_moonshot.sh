16107aae5c7454ac62a05aab67a8603d94e72442
Michel Schanen <mschanen@anl.gov>
2022-09-21 09:37:34 -0500

    Update

--- setup_moonshot.sh
+++ setup_moonshot.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-module load intel mpich hdf5
-julia --project -e 'ENV["JULIA_HDF5_PATH"]="/nfs/gce/software/custom/linux-ubuntu18.04-x86_64/hdf5/1.12.1-mpich-3.4.2-intel-parallel-fortran"; ENV["JULIA_MPI_BINARY"]="system"; ENV["JULIA_MPI_PATH"]="/nfs/gce/software/custom/linux-ubuntu18.04-x86_64/mpich/3.4.2-intel"; using Pkg; Pkg.build("MPI"; verbose=true); Pkg.build("HDF5"; verbose=true)'
-julia --project -e 'using MPI ; MPI.install_mpiexecjl(force=true ;destdir=".")'
+export JULIA_HDF5_PATH="/disk/hdf5/hdf5-1.12.2/build/bin"
+export JULIA_MPI_BINARY="system"
+export JULIA_MPI_PATH="/nfs/gce/software/custom/linux-ubuntu22.04-x86_64/spack/opt/spack/linux-ubuntu22.04-x86_64/gcc-11.3.0/openmpi-4.1.3-qrpnszy"
 export OMP_NUM_THREADS=1
+julia --project -e 'using Pkg; Pkg.build("MPI"; verbose=true); Pkg.build("HDF5"; verbose=true)'
+julia --project -e 'using MPI ; MPI.install_mpiexecjl(force=true ;destdir=".")'
