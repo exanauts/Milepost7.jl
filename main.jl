33233144b8a9b5d9b879faeba9c9db16dc9e0482
Michel Schanen <mschanen@anl.gov>
2023-04-25 08:13:55 -0500

    Update README.md

--- main.jl
+++ main.jl
@@ -1,4 +1,5 @@
 using Milepost7
+using MPI
 
 MPI.Init()
 
@@ -15,6 +16,26 @@ if !isinteractive()
     proxal_iter = args["proxal_iter"]
     exatron_inner_iter = args["exatron_inner_iter"]
     exatron_outer_iter = args["exatron_outer_iter"]
-
-    milepost7(case, load, T, K, configuration; profile=profile, rhopq=rhopq, rhova=rhova, proxal_iter=proxal_iter, exatron_inner_iter=exatron_inner_iter, exatron_outer_iter=exatron_outer_iter)
+else
+    # case = "cases/case_ACTIVSg10k.m"
+    case = "cases/case9.m"
+    load = "cases/case9"
+    profile = true
+    T = 2
+    K = 1
+    rhopq = 3e3
+    rhova = 3e4
+    configuration = 4
+    exatron_inner_iter = 400
+    exatron_outer_iter = 1
+    proxal_iter = 2
 end
+
+info = milepost7(
+    case, load, T, K, configuration;
+    profile=profile,
+    rhopq=rhopq, rhova=rhova,
+    proxal_iter=proxal_iter,
+    exatron_inner_iter=exatron_inner_iter,
+    exatron_outer_iter=exatron_outer_iter
+);
